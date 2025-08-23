3cbd95881d8982a4fa51cca363a30221d44bd706
sourav009 <shakaal949@gmail.com>
2025-08-24 02:07:55 +0530

    creating seperate components for differerent information being displayed

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,45 +1,16 @@
 import { Component, signal } from '@angular/core';
+import { Header } from './header/header';
+import { ServerStatus } from './dashboard/server-status/server-status';
+import { Tarffic } from "./dashboard/tarffic/tarffic";
+import { Tickets } from './dashboard/tickets/tickets';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [ Header, ServerStatus, Tarffic, Tickets ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('server-data-monitor');
-
-  dummyTrafficData = [
-    {
-      id: 'd1',
-      value: 433,
-    },
-    {
-      id: 'd2',
-      value: 260,
-    },
-    {
-      id: 'd3',
-      value: 290,
-    },
-    {
-      id: 'd4',
-      value: 410,
-    },
-    {
-      id: 'd5',
-      value: 397,
-    },
-    {
-      id: 'd6',
-      value: 488,
-    },
-    {
-      id: 'd47',
-      value: 589,
-    },
-  ];
-  maxTraffic = Math.max(...this.dummyTrafficData.map((data) => data.value));
-  currentStatus = 'online';
 
 }
